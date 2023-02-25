8c5892464ad940c458298f5df11bb4fd57bacfef
clevessonmendonca <wanderbloks311@gmail.com>
2023-02-25 10:44:17 -0300

    feat: Adding api and interacting with data

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p><span>PokeNext</span> &copy; 2021</p>
+      <p><span>PokeNext</span> &copy; 2023</p>
     </footer>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,44 @@
-export default function Home() {
+import styles from "@/styles/Home.module.css";
+import { GetStaticProps } from "next";
+
+interface PokemonProps {
+  id: number;
+  name: string;
+  url: string;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const maxPokemons = 1251;
+  const api = `https://pokeapi.co/api/v2/pokemon/`;
+
+  const res = await fetch(`${api}?limit=${maxPokemons}`);
+
+  const data = await res.json();
+
+  data.results.forEach((item: PokemonProps, index: number) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results
+    },
+  };
+}
+
+export default function Home({ pokemons }: PokemonProps[]) {
   return (
-    <div>Hello world</div>
-  )
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+      </div>
+      <ul>
+        {pokemons.map((pokemon: PokemonProps) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
